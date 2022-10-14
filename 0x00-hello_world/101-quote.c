494f4f859b203aa39cba992999e8f95d073544c9
Santana-macharia <santanamash@gmail.com>
2022-10-14 06:57:10 -0700

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,24 @@
+#!/bin/bash
+#include<stdio.h>
+
+
+
+void main()
+
+{
+
+	    //variable definition and initialization
+	    //
+	    //    char stringArray[100];
+	    //
+	    //
+	    //
+	    //        printf("Please write something: \n");
+	    //
+	    //            scanf("%s", stringArray);
+	    //
+	    //                
+	    //
+	    //                    printf("You enter the string %s\n", stringArray);
+	    //
+	    //                    }
